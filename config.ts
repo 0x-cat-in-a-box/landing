6332959b062a8f894732e46fc9d6d080b8bd4d83
fantomă <mrfakie789@gmail.com>
2024-04-18 18:00:43 -0400

    update

--- config.ts
+++ config.ts
@@ -1,13 +1,15 @@
 const config = {
   "format": "standard-v1",
   "tree": [
-    "0x0ac582eee205e6aa735d32bd27d3a808bf065c5987ab1d00a75e8bc7b86f57fd",
-    "0x9f4ace091dfcd5aae246a289e8925d8f353ebc470a6ed8c349ddbb03b5bf159d",
-    "0xa0a4283d970b958c4c685cff285f814bf4b30a05deba103edc1c290132818b8c",
+    "0x4551b304f7f7fc1e7ead6c9ec3000d7860c21113570139450671d48dd86a6f86",
+    "0xe9d768c9b6a19dbdae19c1acc5be9cd6e72df3fcddf5d6261c3c3e35527663ba",
+    "0xd75384666e05efa75eb3d0c0278897bde96b8311d37c1bb7d92693d6d512c5c4",
+    "0x7b8c833e40dd72525d5080984a770995f3d009c8c2dd92bbb1817fd4eccdd411",
     "0x358a8c39a78de2429bee20a6faa06e009471428843327e3382266f932e192a0e",
     "0x6851fb5d4521e8675adc478c50b5c0b18ca11fc85d61582995b0efdb5984d366",
     "0xb5c9d3eb1c9e01dd0daa05038ed23066510d6489a9c628f8c065cd07b8263bf9",
     "0x8ad4755b8c49a1f2c9aa5d64545d1642db44482f89fef53698b956dffea3c45b",
+    "0x6e9c8d4b658f3d10a06efdc8cf03835bfb02662d924a8b5d9f386e86bedcd1f7",
     "0x655dceb3bfce2e65d36062771dd993d21833f89ae6d9d06df1a7d80965189eac",
     "0x50a26b658567316f76f7f2021ccbb47291ca3c8d3463777484ccb2f26f847ad1",
     "0x26a088e24b495a126813e4b068a3fcbaffa3e11846db5d11fdb55aa3315a932c",
@@ -19,42 +21,49 @@ const config = {
         "0x1552B1A051430290f1B5E31F156E3CD501f520C3",
         "1000000000000000000000"
       ],
-      "treeIndex": 7
+      "treeIndex": 9
     },
     {
       "value": [
         "0xe32d9D1F1484f57F8b5198f90bcdaBC914de0B5A",
         "1000000000000000000000"
       ],
-      "treeIndex": 6
+      "treeIndex": 7
     },
     {
       "value": [
         "0x858817FF833B5608656B22A1940eE97C7b26134c",
         "1000000000000000000000"
       ],
-      "treeIndex": 5
+      "treeIndex": 6
     },
     {
       "value": [
         "0x921b87310e4DC8827e91938C89eDa521CAd63c3a",
         "1000000000000000000000"
       ],
-      "treeIndex": 9
+      "treeIndex": 11
     },
     {
       "value": [
         "0x020bCdC76C86db34718f0357c35811b147CD866B",
         "1000000000000000000000"
       ],
-      "treeIndex": 8
+      "treeIndex": 10
     },
     {
       "value": [
         "0x93eb6ccF00Bfdb205ab79E824C4855d2ad196a77",
         "1000000000000000000000"
       ],
-      "treeIndex": 10
+      "treeIndex": 12
+    },
+    {
+      "value": [
+        "0x34A0D4402c039FFb469389e88c3b9F06176e954A",
+        "1000000000000000000000"
+      ],
+      "treeIndex": 8
     }
   ],
   "leafEncoding": ["address", "uint256"]
